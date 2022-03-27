960682656be2da34a5ad87a8cc40d703fa7dc1ab
Acer <Acer@DESKTOP-65H8V6H>
2022-03-27 10:02:41 -0100

    first commit

---
+++ javaProgramming/src/Unit_01/P1_Task01_FirstJavaPrograminJava.java
@@ -0,0 +1,9 @@
+package Unit_01;
+
+public class P1_Task01_FirstJavaPrograminJava {
+
+	public static void main(String[] args)
+	{
+		System.out.print("Hello World!");
+	}
+}
---
+++ javaProgramming/src/Unit_01/P2_Task01_KeywordsinJava.java
@@ -0,0 +1,27 @@
+package Unit_01;
+
+public class P2_Task01_KeywordsinJava {
+
+	public Static Void main(String[] args) {
+		//Syntax error on token "float", invalid VariableDeclaratorId
+		int float=23;
+		//multiple markers at this line
+		//- Main cannot be resolved to a 
+		 //variable
+		Main int=new Main();
+		//Syntax error on token "int", invalid Expression
+		System.out.println(int.a);
+		}
+		//Multiple markers at this line
+		//- Syntax error, insert ";" to complete 
+		// MethodDeclaration
+		//- Syntax error on token "int", Identifier expected
+		//Syntax error on token "int", Identifier expected
+		void int() {
+			int a=10;
+		}
+		public class Main{
+			int a=1;
+		   
+		}
+}
